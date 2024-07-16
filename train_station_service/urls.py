5b1a05e3b5c8b1c120a679e5e93898fad0dd20a7
Vadim Babii <judviiioff@gmail.com>
2024-07-16 14:44:53 +0300

    Implement debug toolbar for project, prepare settings.py for work with user and train station apps, refactoring train_station_service/

--- train_station_service/urls.py
+++ train_station_service/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from train_station import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "api/train-station/",
+        include("train_station.urls"),
+        name="train-station"),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
