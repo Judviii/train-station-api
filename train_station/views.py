7ea006d8ffd9852aaf6fdc86ad8f10f989266167
Vadim Babii <judviiioff@gmail.com>
2024-07-19 01:04:57 +0300

    implement basic views for all models, configure URL routing, and integrate serializers

--- train_station/views.py
+++ train_station/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from train_station.models import (
+    Station,
+    Route,
+    Ticket,
+    Order,
+    TrainType,
+    Train,
+    Crew,
+    Journey,
+)
+
+from train_station.serializers import (
+    StationSerializer,
+    RouteSerializer,
+    TrainSerializer,
+    OrderSerializer,
+    TrainTypeSerializer,
+    TicketSerializer,
+    CrewSerializer,
+    JourneySerializer,
+    JourneyListSerializer,
+    JourneyDetailSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+    TrainListSerializer,
+    TrainDetailSerializer,
+    TicketListSerializer,
+)
+
+
+class StationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(
+    ModelViewSet
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        return RouteSerializer
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketViewSet(ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TicketListSerializer
+        return TicketSerializer
+
+
+class TrainTypeViewSet(ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TrainListSerializer
+        if self.action == "retrieve":
+            return TrainDetailSerializer
+        return TrainSerializer
+
+
+class CrewViewSet(ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class JourneyViewSet(ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return JourneyListSerializer
+
+        if self.action == "retrieve":
+            return JourneyDetailSerializer
+
+        return JourneySerializer
+
